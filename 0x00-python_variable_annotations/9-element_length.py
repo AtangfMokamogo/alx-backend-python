d2ff487b033910cb8884da8057228db16ddaab01
Atang F Mokamogo <atangfmokamogo@outlook.com>
2023-08-03 23:44:46 +0200

    added correction to answer file for task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ This is an implementation of basic type annotations in Python """
-from typing import List, Tuple
+from typing import Sequence, Iterable, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ This function calculates the length of each element in the input list
 
     Args:
